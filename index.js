80ec4e453efc8ea9b3d076ca2404e0d5e82efcec
Shahzad <mech1090@gmail.com>
2022-04-27 22:05:28 -0600

    model created

--- index.js
+++ index.js
@@ -12,22 +12,20 @@ require('./db')
 const app = express()
 
 //middlewares
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
-
 app.use('/products',productRouter)
 
 //templete engine EJS
-
 app.set('view engine','ejs')
 app.set('view','./views')
 
 //start server
 port = config.get('port') || 8080
 
+// Establishing connection with db
 mongoose.connection.once('open',()=>{
     app.listen(port, ()=>{
         console.log(`Server running on port ${port}`)
---
+++ models/product.model.js
@@ -0,0 +1,25 @@
+const { string, number } = require('joi')
+const mongoose = require('mongoose')
+
+const schemaDefination = {
+    name:{
+        type:String,
+        required:[true,'Name is required']
+    },
+    specs:{
+        type:String
+    },
+    Price:{
+        type:number,
+        required:[true,'Price is required']
+    },
+    instock:{
+        type:Boolean,
+        required:[true,'Stock status is required']
+
+    }
+}
+
+const productSchema = new mongoose.Schema(schemaDefination)
+const Product = mongoose.model('Product',productSchema)
+module.exports = Product
